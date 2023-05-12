9f049841ac31f1626f611c5ff856d8cbdec7f986
MehmetTop360 <mehmettop2003@gmail.com>
2023-05-12 21:08:24 +0300

    created game.py and did issue 1

---
+++ game.py
@@ -0,0 +1,11 @@
+import random
+
+def initialize_deck():
+    suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+    deck = [(rank, suit) for suit in suits for rank in ranks]
+    return deck
+
+def shuffle_deck(deck):
+    random.shuffle(deck)
+    return deck
